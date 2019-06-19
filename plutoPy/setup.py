df978a876eefb4281400efca256ac6bf93ace11d
Shyam Sunder <shyam.udupa@gmail.com>
2019-06-19 11:21:49 +0530

    changed package name

--- plutoPy/setup.py
+++ plutoPy/setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="plutoPy-shyams80",
+    name="plutoPy",
     version="0.1.0",
     author="shyam",
     author_email="shyam.udupa@gmail.com",
